ca78c90e44b6d5d4161e4cd0173026236eb7ec1c
ocelotTKB <somaisapen1218@gmail.com>
2021-02-06 02:31:10 +0900

    edit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const Discord = require('discord.js');
+const { prototype } = require('events');
 const client = new Discord.Client();
 const fs = require('fs');
 const config = JSON.parse(fs.readFileSync("./config.json"));
+Array.prototype.random = function(){ return this[Math.floor(Math.random() * this.length)]}
+const meigen = ["社会的に殺される", "おらいぐよ！！！！", "IMIちゃん"];
+
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -9,7 +13,9 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.content === 'ping') {
-        msg.reply('Pong!');
+        msg.reply(meigen.random());
+        msg.react('🤔')
+        msg.pin({ reason: 'important' });
     }
 });
 
